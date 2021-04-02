bce85b6549225a90759c373b8fc73d55b01a3edd
pxydn <pxydn@protonmail.com>
2021-04-02 20:11:57 +0100

    playing around with geist and updated packages

--- packages/client/pages/index.js
+++ packages/client/pages/index.js
@@ -1,14 +1,21 @@
-import { Page, Text } from '@geist-ui/react'
+import { Page, Text, Button, Input, Spacer } from '@geist-ui/react'
 
 export default function Home() {
   return (
     <Page dotBackdrop size="mini">
       <Page.Header>
-        <Text h2>React Application with Geist UI</Text>
+        <Text h2>Metaverse Pool</Text>
       </Page.Header>
       <Text>
-        Hello, I am using <Text b>Gesit UI</Text> !
+        Hello, I am using <Text b>Geist UI</Text>!
       </Text>
+      <Input placeholder="Stacks address"></Input>
+      <Spacer y={0.5}></Spacer>
+      <Input placeholder="sats to add to pool"></Input>
+      <Button>Action</Button>
+      <Page.Footer>
+        <Text small>this is a footer</Text>
+      </Page.Footer>
     </Page>
   )
 }
