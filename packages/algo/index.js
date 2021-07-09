b65514011cf81b2a1a681a4c280bf41480f4d4fa
asteria <asteria@syvita.org>
2021-07-09 16:38:04 +0000

    please work

--- packages/algo/index.js
+++ packages/algo/index.js
@@ -1,21 +1,13 @@
 import { Router } from 'itty-router'
+import { updatePrice } from './src/updatePrice'
 
 // Create a new router
 const router = Router()
 
-/*
-Our index route, a simple hello world.
-*/
 router.get("/", () => {
   return new Response("Hello, world! This is the root page of your Worker template.")
 })
 
-/*
-This route demonstrates path parameters, allowing you to extract fragments from the request
-URL.
-
-Try visit /example/hello and see the response.
-*/
 router.get("/example/:text", ({ params }) => {
   // Decode text like "Hello%20world" into "Hello world"
   let input = decodeURIComponent(params.text)
@@ -34,49 +26,42 @@ router.get("/example/:text", ({ params }) => {
   })
 })
 
-/*
-This shows a different HTTP method, a POST.
-
-Try send a POST request using curl or another tool.
-
-Try the below curl command to send JSON:
-
-$ curl -X POST <worker> -H "Content-Type: application/json" -d '{"abc": "def"}'
-*/
-router.post("/post", async request => {
-  // Create a base object with some fields.
-  let fields = {
-    "asn": request.cf.asn,
-    "colo": request.cf.colo
-  }
-
-  // If the POST data is JSON then attach it to our response.
-  if (request.headers.get("Content-Type") === "application/json") {
-    fields["json"] = await request.json()
-  }
-
-  // Serialise the JSON to a string.
-  const returnData = JSON.stringify(fields, null, 2);
-
-  return new Response(returnData, {
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-})
-
-/*
-This is the last route we define, it will match anything that hasn't hit a route we've defined
-above, therefore it's useful as a 404 (and avoids us hitting worker exceptions, so make sure to include it!).
-
-Visit any page that doesn't exist (e.g. /foobar) to see it in action.
-*/
 router.all("*", () => new Response("404, not found!", { status: 404 }))
 
-/*
-This snippet ties our worker to the router we deifned above, all incoming requests
-are passed to the router where your routes are called and the response is sent.
-*/
+async function handleScheduled(event) {
+  cron = event.cron
+
+  if (cron == '* * * * *') {
+    // we need to get the latest stx/btc price and add to this month's avg
+    const now = new Date()
+    const key = now.getMonth() + '-' + now.getFullYear()
+    const value = await KV.get(key)
+
+    if (value == null) {
+      const newMonth = {
+        price: {
+          currentAvg: 0,
+          count: 0,
+          allPrices: []
+        }
+      }
+      updatePrice(key, newMonth)
+    } else {
+      updatePrice(key, value)
+    }
+
+  } else if (cron == '*/10 * * * *') {
+    // we need to get latest mining
+
+  } else if (cron == '') {
+    // we need to publish latest cycle targets to contract
+  }
+}
+
 addEventListener('fetch', (e) => {
   e.respondWith(router.handle(e.request))
+})
+
+addEventListener('scheduled', event => {
+  event.waitUntil(handleScheduled(event))
 })
