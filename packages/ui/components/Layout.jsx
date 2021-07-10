699489ee8181df42b66eb6dd3c09190836bbe1f3
asteria <asteria@syvita.org>
2021-07-10 18:47:18 +0100

    update global layout

--- packages/ui/components/Layout.jsx
+++ packages/ui/components/Layout.jsx
@@ -1,8 +1,12 @@
 import Navbar from "./Navbar";
+import Head from 'next/head'
 
 const Layout = ({ children }) => {
   return (
     <div>
+      <Head>
+      <script async src="https://arc.io/widget.min.js#3rsnfHXA"></script>
+    </Head>
       <Navbar />
       {children}
     </div>
