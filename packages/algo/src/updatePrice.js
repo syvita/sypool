4d27234adc64a6683d80cb354ee04674d3078697
asteria <asteria@syvita.org>
2021-07-12 18:46:04 +0100

    Update updatePrice.js

--- packages/algo/src/updatePrice.js
+++ packages/algo/src/updatePrice.js
@@ -1,28 +1,28 @@
 export async function updatePrice(key, value) {
     const parsedValue = JSON.parse(value)
-  
+
     const url = 'https://api.coingecko.com/api/v3/simple/price?ids=blockstack&vs_currencies=btc'
     const init = {
-      headers: {
-        "content-type": "application/json;charset=UTF-8",
-      },
+        headers: {
+            "content-type": "application/json;charset=UTF-8",
+        },
     }
     const response = await fetch(url, init)
     const result = await response.json()
     const currentPrice = result.blockstack.btc * 100000000
-  
-    const newAvg = (currentPrice / 43830) + parsedValue.averagePrice
+
+    const newAvg = (currentPrice / 43830) + parsedValue.price.currentAvg
     let newValue = parsedValue
-  
+
     newValue.price.currentAvg = newAvg
     newValue.price.count++
-    newValue.price.allPrices.push(currentPrice)
-  
+        newValue.price.allPrices.push(currentPrice)
+
     newValue = JSON.stringify(newValue)
     await KV.put(key, newValue, {
-      metadata: {
-        currentAvg: newAvg,
-        count: newValue.price.count
-      }
+        metadata: {
+            currentAvg: newAvg,
+            count: newValue.price.count
+        }
     })
-  }
+}
