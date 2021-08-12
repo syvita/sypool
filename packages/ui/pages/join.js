b6774b2bb44fdb264b28b889b9b8b11f90e72825
SyInvidia <paradinight@protonmail.com>
2021-08-12 02:35:13 +0100

    Fixing Join page and hamburger

--- packages/ui/pages/join.js
+++ packages/ui/pages/join.js
@@ -1,16 +1,21 @@
 import { useConnect } from "../lib/auth";
+import { useContext } from "react";
+import { ThemeContext } from "../components/Layout";
 import styles from "../styles/Join.module.css";
 
 const Join = () => {
   const { handleOpenAuth } = useConnect();
+  const { theme } = useContext(ThemeContext);
   return (
-    <main>
-      <div className={styles.blobGreen}></div>
-      <div className={styles.connect}>
-        <h1>Connect to Sypool</h1>
-        <button onClick={handleOpenAuth}>Use Stacks Connect</button>
-      </div>
-    </main>
+    <div className={theme === "light" ? styles.light : styles.dark}>
+      <main className={styles.join}>
+        <div className={styles.blobGreen}></div>
+        <div className={styles.connect}>
+          <h1>Connect to Sypool</h1>
+          <button onClick={handleOpenAuth}>Use Stacks Connect</button>
+        </div>
+      </main>
+    </div>
   );
 };
 
